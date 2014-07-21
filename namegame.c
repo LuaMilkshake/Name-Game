28f712ce3990e5d2108cc42272bbe9278c179e88
John Marion <john@lmsn.net>
2014-07-21 02:58:37 -0700

    Add first implementation of name game

--- namegame.c
+++ namegame.c
@@ -9,10 +9,41 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
+	int i = 1;
+
 	if(argc == 1){
 		printf("Usage : %s <name> ...\n", argv[0]);
 		return 0;
 	}
+
+	for(;i<argc;i++){
+		printf("%s!\n", argv[i]);
+
+		/*
+		 * TODO: Fix issues with vowels, ie Arnold becomes "brnold". This is
+		 * errata in the original definition, which claims the replacemnt of B, F,
+		 * and M is the "only rule that is contrary".
+		 */
+
+		if(tolower(*argv[i]) == 'b')
+			printf("%s, %s bo-%s\n", argv[i], argv[i], argv[i] + 1);
+		else
+			printf("%s, %s bo b%s\n", argv[i], argv[i], argv[i] + 1);
+
+		if(tolower(*argv[i]) == 'f')
+			printf("Bonana fanna fo-%s\n", argv[i] + 1);
+		else
+			printf("Bonana fanna fo f%s\n", argv[i] + 1);
+
+		if(tolower(*argv[i]) == 'm')
+			printf("Fe fi mo-%s\n", argv[i] + 1);
+		else
+			printf("Fe fi mo m%s\n", argv[i] + 1);
+
+		printf("%s!\n\n", argv[i]);
+	}
+
+	puts("The name game!\n");
 
 	return 0;
 }
