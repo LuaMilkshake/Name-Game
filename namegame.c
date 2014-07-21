1ff961507cf89240566391346de09572e41c1b1a
John Marion <john@lmsn.net>
2014-07-21 01:27:22 -0700

    Create namegame.c with boilerplate C program

---
+++ namegame.c
@@ -0,0 +1,11 @@
+/**
+ * The Name Game
+ * 2014 John "LuaMilkshake" Marion
+ *
+ * A C implementation of the "Name Game" as defined in the 1964 Shirley Ellis
+ * song by the same name.
+ **/
+
+int main(int argc, char** argv){
+	return 0;
+}
