42313184cf5562649eadc178a215debb0498d1b7
John Marion <john@lmsn.net>
2014-07-31 05:11:38 -0700

    Add case for names starting with vowels (fix #2)

--- namegame.c
+++ namegame.c
@@ -11,6 +11,8 @@
 
 int main(int argc, char** argv){
 	int i = 1;
+	/* Points to the name sans the first letter (with exceptions) */
+	char* cut_name = NULL;
 
 	if(argc == 1){
 		printf("Usage : %s <name> ...\n", argv[0]);
@@ -28,20 +30,36 @@ int main(int argc, char** argv){
 
 		printf("%s!\n", argv[i]);
 
+		/* Remove the first letter of the name, unless it's a vowel. This is
+		 * not specified explicitly in the original definition but was added
+		 * so names like "Erin" don't become "brin".
+		 */
+		switch(tolower(*argv[i])){
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				cut_name = argv[i];
+				break;
+			default:
+				cut_name = argv[i] + 1;
+		}
+
 		if(tolower(*argv[i]) == 'b')
-			printf("%s, %s bo-%s\n", argv[i], argv[i], argv[i] + 1);
+			printf("%s, %s bo-%s\n", argv[i], argv[i], cut_name);
 		else
-			printf("%s, %s bo b%s\n", argv[i], argv[i], argv[i] + 1);
+			printf("%s, %s bo b%s\n", argv[i], argv[i], cut_name);
 
 		if(tolower(*argv[i]) == 'f')
-			printf("Bonana fanna fo-%s\n", argv[i] + 1);
+			printf("Bonana fanna fo-%s\n", cut_name);
 		else
-			printf("Bonana fanna fo f%s\n", argv[i] + 1);
+			printf("Bonana fanna fo f%s\n", cut_name);
 
 		if(tolower(*argv[i]) == 'm')
-			printf("Fe fi mo-%s\n", argv[i] + 1);
+			printf("Fe fi mo-%s\n", cut_name);
 		else
-			printf("Fe fi mo m%s\n", argv[i] + 1);
+			printf("Fe fi mo m%s\n", cut_name);
 
 		printf("%s!\n\n", argv[i]);
 	}
