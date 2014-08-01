e85f743e9a46f7740dffc7fdeca473edd106054a
John Marion <john@lmsn.net>
2014-08-01 04:53:56 -0700

    Rename i var to arg_i, to allow other iterators

--- namegame.c
+++ namegame.c
@@ -10,7 +10,8 @@
 #include <ctype.h>
 
 int main(int argc, char** argv){
-	int i = 1;
+	/* Iterators */
+	int arg_i = 1;
 	/* Points to the name sans the first letter (with exceptions) */
 	char* cut_name = NULL;
 
@@ -19,49 +20,49 @@ int main(int argc, char** argv){
 		return 0;
 	}
 
-	for(;i<argc;i++){
+	for(;arg_i<argc;arg_i++){
 		/* Test to see if the arg is either an empty string, or only one char */
-		if(*argv[i] == 0){
+		if(*argv[arg_i] == 0){
 			continue;
-		}else if(argv[i][1] == 0){
-			fprintf(stderr, "\'%c\' is too short to use as a name!\n\n", *argv[i]);
+		}else if(argv[arg_i][1] == 0){
+			fprintf(stderr, "\'%c\' is too short to use as a name!\n\n", *argv[arg_i]);
 			continue;
 		}
 
-		printf("%s!\n", argv[i]);
+		printf("%s!\n", argv[arg_i]);
 
 		/* Remove the first letter of the name, unless it's a vowel. This is
 		 * not specified explicitly in the original definition but was added
 		 * so names like "Erin" don't become "brin".
 		 */
-		switch(tolower(*argv[i])){
+		switch(tolower(*argv[arg_i])){
 			case 'a':
 			case 'e':
 			case 'i':
 			case 'o':
 			case 'u':
-				cut_name = argv[i];
+				cut_name = argv[arg_i];
 				break;
 			default:
-				cut_name = argv[i] + 1;
+				cut_name = argv[arg_i] + 1;
 		}
 
-		if(tolower(*argv[i]) == 'b')
-			printf("%s, %s bo-%s\n", argv[i], argv[i], cut_name);
+		if(tolower(*argv[arg_i]) == 'b')
+			printf("%s, %s bo-%s\n", argv[arg_i], argv[arg_i], cut_name);
 		else
-			printf("%s, %s bo b%s\n", argv[i], argv[i], cut_name);
+			printf("%s, %s bo b%s\n", argv[arg_i], argv[arg_i], cut_name);
 
-		if(tolower(*argv[i]) == 'f')
+		if(tolower(*argv[arg_i]) == 'f')
 			printf("Bonana fanna fo-%s\n", cut_name);
 		else
 			printf("Bonana fanna fo f%s\n", cut_name);
 
-		if(tolower(*argv[i]) == 'm')
+		if(tolower(*argv[arg_i]) == 'm')
 			printf("Fe fi mo-%s\n", cut_name);
 		else
 			printf("Fe fi mo m%s\n", cut_name);
 
-		printf("%s!\n\n", argv[i]);
+		printf("%s!\n\n", argv[arg_i]);
 	}
 
 	puts("The name game!\n");
